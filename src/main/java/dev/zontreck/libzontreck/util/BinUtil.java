48a4e126cbb841c8a66ee6af9cc9e834aded274e
Aria <tarapiccari@gmail.com>
2023-02-22 01:33:03 -0700

    Stage changes pre-downgrade to 1.18.2

--- src/main/java/dev/zontreck/libzontreck/util/BinUtil.java
+++ src/main/java/dev/zontreck/libzontreck/util/BinUtil.java
@@ -3,6 +3,11 @@ package dev.zontreck.libzontreck.util;
 public class BinUtil {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
+    /**
+     * Converts a byte array to hexadecimal
+     * @param bytes
+     * @return
+     */
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
